82088ef17f8fdca3760449296b9c6fe16a6f7b1a
gngnk123 <ngongadze2000@gmail.com>
2022-11-16 13:29:08 +0400

    done

---
+++ source/repos/Car_Details/Car_Details/Controllers/CarBrandsController.cs
@@ -0,0 +1,22 @@
+﻿using Car_Details.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Car_Details.Controllers
+{
+    public class CarBrandsController : Controller
+    {
+        public IActionResult EnterModels()
+        {
+            CarBrandsModel model = new CarBrandsModel();
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Details(CarBrandsModel model)
+        {
+            //string Details = model.Br_Name +"   "+ model.Model + "   " + model.year;
+            string details = model.Br_Name;
+   
+            return PartialView("Details", details);
+        }
+    }
+}
---
+++ source/repos/Car_Details/Car_Details/Models/CarBrandsModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Car_Details.Models
+{
+    public class CarBrandsModel
+    {
+        public int Id { get; set; }
+        public int Year { get; set; }
+        public string? Br_Name { get; set; }
+        public string ?Model { get; set; }
+    }
+}
